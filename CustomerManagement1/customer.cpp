f1977fc657db3118ab75e811696bdb96d3a2979f
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-10-21 12:51:50 -0700

    Initial commit: Implemented Customer class with ID, name, and email attributes, along with methods for input/output handling. Added main program to prompt user for customer details.

---
+++ CustomerManagement1/customer.cpp
@@ -0,0 +1,8 @@
+//
+//  customer.cpp
+//  CustomerManagement1
+//
+//  Created by Isaac Adeleke on 10/21/24.
+//
+
+#include "customer.hpp"
---
+++ CustomerManagement1/customer.hpp
@@ -0,0 +1,13 @@
+//
+//  customer.hpp
+//  CustomerManagement1
+//
+//  Created by Isaac Adeleke on 10/21/24.
+//
+
+#ifndef customer_hpp
+#define customer_hpp
+
+#include <stdio.h>
+
+#endif /* customer_hpp */
